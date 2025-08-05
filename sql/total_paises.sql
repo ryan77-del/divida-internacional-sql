2456c655523aef0e445085559d82739c2d1c5eef
ryan77-del <ryanzika77@gmail.com>
2025-08-05 11:35:30 -0300

    Update total_paises.sql

--- sql/total_paises.sql
+++ sql/total_paises.sql
@@ -1,4 +1,3 @@
--- Quantos países distintos estão presentes na base de dados? --
-SELECT 
-    COUNT(DISTINCT country) AS total_paises
+-- Quantos países distintos estão presentes na base de dados?
+SELECT COUNT(DISTINCT country) AS total_paises
 FROM debt;
